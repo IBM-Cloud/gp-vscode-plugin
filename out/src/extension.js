0a942341f868ba0b67fb0174ba907d68bdb953f5
Steven Atkin <atkin@us.ibm.com>
2016-08-19 11:14:12 -0400

    Update out folder

--- out/src/extension.js
+++ out/src/extension.js
@@ -1,6 +1,22 @@
+/*
+ * Copyright IBM Corp. 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 "use strict";
-// The module 'vscode' contains the VS Code extensibility API
-// Import the necessary extensibility types to use in your code below
+/**
+ * @author Steven Atkin
+ */
 var nls = require('vscode-nls');
 var path = require('path');
 var vscode_1 = require('vscode');
@@ -8,8 +24,6 @@ var localize = nls.config({ locale: vscode_1.env.language })(path.join(__dirname
 // This method is called when your extension is activated. Activation is
 // controlled by the activation events defined in package.json.
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error).
-    // This line of code will only be executed once when your extension is activated.
     console.log('Globalization Pipeline plugin is now active!');
     // start the Globalization Pipeline plugin
     var g11n = new GlobalizationPipeline();
