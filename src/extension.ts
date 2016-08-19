9123ac359c9107b6a2bc6f04d2ed6ac284b85f6a
Steven Atkin <atkin@us.ibm.com>
2016-08-19 10:07:18 -0400

    Added locale detection

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,19 @@
 // Import the necessary extensibility types to use in your code below
 import * as nls from 'vscode-nls';
 import * as path from 'path';
-import {window, workspace, commands, Disposable, ExtensionContext, 
-    StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
-let localize:any = nls.config()(path.join(__dirname, 'data'));
+import {
+    window,
+    workspace,
+    commands,
+    Disposable,
+    ExtensionContext,
+    StatusBarAlignment,
+    StatusBarItem,
+    TextDocument,
+    env
+} from 'vscode';
+let localize: any = nls.config({locale: env.language})(path.join(__dirname, 'data'));
+
 
 // This method is called when your extension is activated. Activation is
 // controlled by the activation events defined in package.json.
@@ -57,10 +67,10 @@ class Settings {
     private _targetLanguages: string[];
     private _sourceLanguage: string;
 
-    constructor(username: string, 
-        password: string, 
-        instance: string, 
-        url: string, 
+    constructor(username: string,
+        password: string,
+        instance: string,
+        url: string,
         target: string[],
         source: string) {
         this._userId = username;
@@ -91,7 +101,7 @@ class Settings {
         return this._targetLanguages;
     }
 
-    get sourceLanguage() : string {
+    get sourceLanguage(): string {
         return this._sourceLanguage;
     }
 }
@@ -101,7 +111,7 @@ class GlobalizationPipeline {
     private _userSettings: Settings;
 
     constructor() {
-        this._userSettings= getConfigurationSettings();
+        this._userSettings = getConfigurationSettings();
         let credentials = {
             credentials: {
                 url: this._userSettings.url,
@@ -131,8 +141,7 @@ class GlobalizationPipeline {
             if (languageType == "json") {
                 try {
                     parsed = JSON.parse(docContent);
-                }
-                catch (e) {
+                } catch (e) {
                     parsed = {};
                 }
             }
@@ -141,12 +150,10 @@ class GlobalizationPipeline {
                 let props = require("properties-parser");
                 try {
                     parsed = props.parse(docContent);
-                }
-                catch (e) {
+                } catch (e) {
                     parsed = {};
                 }
-            }
-            else if(languageType == 'javascript') {
+            } else if (languageType == 'javascript') {
                 try {
                     let esprima = require('esprima');
 
@@ -157,7 +164,7 @@ class GlobalizationPipeline {
                     });
 
                     // grab the define function
-                    let defines = ast.body.filter(function(node){
+                    let defines = ast.body.filter(function(node) {
                         return node.expression.callee.name === 'define';
                     })[0];
 
@@ -165,23 +172,22 @@ class GlobalizationPipeline {
                     let args = defines.expression['arguments'];
 
                     // Grab the root object of the bundle
-                    let bundle = args.filter(function(arg){
+                    let bundle = args.filter(function(arg) {
                         return arg.type === 'ObjectExpression';
                     })[0];
 
                     // Grab the array of key value pairs
                     let items = bundle.properties[0].value.properties;
-                    for(let item of items) {
+                    for (let item of items) {
                         // add the key value pair to the json object
                         parsed[item.key.name] = item.value.value;
                     }
-                }
-                catch(e) {
+                } catch (e) {
                     console.log(e);
                     parsed = {};
-                } 
+                }
             }
-            
+
         }
         return parsed;
     }
@@ -190,32 +196,29 @@ class GlobalizationPipeline {
         // We need to do this so we can access the object from the lambda functions
         var _this = this;
 
-        this.g11n.bundles({}, function (err, bundles) {
+        this.g11n.bundles({}, function(err, bundles) {
             if (err) {
-                window.showErrorMessage(localize(0,null));
+                window.showErrorMessage(localize(0, null));
                 return;
-            }
-            else {
+            } else {
                 var bundleList = Object.keys(bundles);
 
-                window.setStatusBarMessage(localize(1,null), 2000);
+                window.setStatusBarMessage(localize(2, null), 2000);
 
                 window.showQuickPick(bundleList, {
-                    placeHolder: localize(2,null)
+                    placeHolder: localize(2, null)
                 }).then(bundleName => {
                     if (typeof bundleName !== "string") {
                         return;
-                    }
-                    else {
+                    } else {
                         _this.g11n.bundle(bundleName).delete({},
-                        function(err, results){
-                            if (err) {
-                                    window.showErrorMessage(localize(3,null) + bundleName);
-                            }
-                            else if (results.status == "SUCCESS"){
-                                    window.showInformationMessage(localize(4,null) + bundleName);
-                            }
-                        });
+                            function(err, results) {
+                                if (err) {
+                                    window.showErrorMessage(localize(3, null) + bundleName);
+                                } else if (results.status == "SUCCESS") {
+                                    window.showInformationMessage(localize(4, null) + bundleName);
+                                }
+                            });
                     }
                 });
             }
@@ -226,43 +229,40 @@ class GlobalizationPipeline {
         // We need to do this so we can access the object from the lambda functions
         var _this = this;
 
-        this.g11n.bundles({}, function (err, bundles) {
+        this.g11n.bundles({}, function(err, bundles) {
             if (err) {
-                window.showErrorMessage(localize(0,null));
+                window.showErrorMessage(localize(0, null));
                 return;
-            }
-            else {
+            } else {
                 var bundleList = Object.keys(bundles);
 
-                window.setStatusBarMessage(localize(1,null), 2000);
+                window.setStatusBarMessage(localize(1, null), 2000);
 
                 window.showQuickPick(bundleList, {
-                    placeHolder: localize(5,null)
+                    placeHolder: localize(5, null)
                 }).then(bundleName => {
                     if (typeof bundleName !== "string") {
                         return;
-                    }
-                    else {
+                    } else {
                         let parsed = _this.parseContent();
                         let length = Object.keys(parsed).length;
 
                         // we have a set of key value pairs to upload
-                        if(length > 0) {
+                        if (length > 0) {
                             _this.g11n.bundle(bundleName).uploadStrings({
-                            languageId: _this._userSettings.sourceLanguage,
-                            strings: parsed
-                            }, function (err, results) {
+                                languageId: _this._userSettings.sourceLanguage,
+                                strings: parsed
+                            }, function(err, results) {
                                 if (err) {
-                                    window.showErrorMessage(localize(6,null) + bundleName);
-                                }
-                                else if (results.status == "SUCCESS"){
-                                    window.showInformationMessage(localize(7,null) + bundleName);
+                                    window.showErrorMessage(localize(6, null) + bundleName);
+                                } else if (results.status == "SUCCESS") {
+                                    window.showInformationMessage(localize(7, null) + bundleName);
                                 }
                             });
                         }
                         // no key value pairs to upload
                         else {
-                            window.showErrorMessage(localize(8,null) + bundleName);
+                            window.showErrorMessage(localize(8, null) + bundleName);
                             return;
                         }
                     }
@@ -275,31 +275,29 @@ class GlobalizationPipeline {
     public createBundle() {
         // Ask for the name of the bundle to create
         window.showInputBox({
-            prompt: localize(13,null),
+            prompt: localize(13, null),
             validateInput: text => {
                 //let regex = /\s/g;
                 let regex = /[a-zA-Z0-9][a-zA-Z0-9_.\\-]+/;
-                return regex.test(text) ? '' : localize(9,null);
+                return regex.test(text) ? '' : localize(9, null);
             }
         }).then(bundleName => {
             // make sure we have a valid bundle name
             if (typeof bundleName !== "string") {
                 return;
-            }
-            else {
+            } else {
                 // Create the bundle
-                window.showInformationMessage(localize(10,null) + bundleName);
+                window.showInformationMessage(localize(10, null) + bundleName);
                 // Call the pipeline service on Bluemix
-                this.g11n.bundle(bundleName).create({ 
+                this.g11n.bundle(bundleName).create({
                     sourceLanguage: this._userSettings.sourceLanguage,
-                    targetLanguages: this._userSettings.targetLanguages 
-                    }, function (err, results) {
-                        if (err) {
-                            window.showErrorMessage(localize(11,null) + bundleName);
-                        }
-                        else if (results.status == 'SUCCESS') {
-                            window.showInformationMessage(localize(12,null) + bundleName);
-                        }   
+                    targetLanguages: this._userSettings.targetLanguages
+                }, function(err, results) {
+                    if (err) {
+                        window.showErrorMessage(localize(11, null) + bundleName);
+                    } else if (results.status == 'SUCCESS') {
+                        window.showInformationMessage(localize(12, null) + bundleName);
+                    }
                 });
             }
         });
@@ -307,6 +305,6 @@ class GlobalizationPipeline {
 
 
     dispose() {
-        
+
     }
 }
